e327bc4ca0209d4b7e8ac915581710bb69b73352
Full-Hat <68519677+Full-Hat@users.noreply.github.com>
2021-06-26 14:15:15 +0300

    Update CCNET.cpp

--- CCNET.cpp
+++ CCNET.cpp
@@ -44,3 +44,59 @@ port_CCNET::~port_CCNET()
     CCValidator.disable_sequence();
     std::this_thread::sleep_for(std::chrono::seconds(delay_stop_listening));
 }
+
+/// Static version
+
+cash_code_protocol port_CCNET_static::CCValidator;
+std::string port_CCNET_static::port_path;
+
+// time settings (sec)
+const int port_CCNET_static::delay_power_up;
+const int port_CCNET_static::delay_start_listening;
+const int port_CCNET_static::delay_stop_listening;
+
+void port_CCNET_static::init(const std::string &port_path)
+{
+    using port = port_CCNET_static;
+
+    port::port_path = port_path;
+    // power up validator
+    port::CCValidator.connect_validator(port::port_path.c_str());
+    port::CCValidator.power_up_validator();
+    std::this_thread::sleep_for(std::chrono::seconds(port::delay_power_up));
+}
+
+void port_CCNET_static::destroy()
+{
+    using port = port_CCNET_static;
+
+    port::CCValidator.disable_sequence();
+    std::this_thread::sleep_for(std::chrono::seconds(port::delay_stop_listening));
+}
+
+int port_CCNET_static::get_total_cash()
+{
+    return port_CCNET_static::CCValidator.m_cash_received;
+}
+
+int port_CCNET_static::listen_port()
+{
+    return port_CCNET_static::CCValidator.validator_listener();
+}
+
+void port_CCNET_static::on()
+{
+    using port = port_CCNET_static;
+
+    // start listening
+    port::CCValidator.enable_sequence();
+    std::this_thread::sleep_for(std::chrono::seconds(port::delay_start_listening));
+}
+
+void port_CCNET_static::off()
+{
+    using port = port_CCNET_static;
+
+    port::CCValidator.disable_sequence();
+    std::this_thread::sleep_for(std::chrono::seconds(port::delay_stop_listening));
+}
